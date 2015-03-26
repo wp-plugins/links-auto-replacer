e59301f2541f5baba2a72a70432d9a161a85b408
waseem_senjer <waseem_senjer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 10:01:07 +0000

    rollback
    git-svn-id: https://plugins.svn.wordpress.org/links-auto-replacer/trunk@1121217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lar-links-auto-replacer.php
+++ lar-links-auto-replacer.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Links Auto Replacer
  * Plugin URI:        http://waseem-senjer.com/lar/
  * Description:       Auto replace your affiliate links and track them.
- * Version:           1.1.3
+ * Version:           1.1.2
  * Author:            Waseem Senjer
  * Author URI:        http://waseem-senjer.com
  * Text Domain:       lar-links-auto-replacer
@@ -115,9 +115,9 @@ function lar_auto_replace_links($content){
 		
 		$keywords = explode(',', $link->keyword);
 		foreach($keywords as $keyword){
-			$final_url = ' <a href="'.$url.'" '.$dofollow.' target="'.$link->open_in.'">'.$keyword.'</a>';
+			$final_url = ' <a href="'.$url.'" '.$dofollow.' target="'.$link->open_in.'">'.$keyword.'</a> ';
 			$post_content = $content;
-			$content =  preg_replace('/\s'.$keyword.'/iu', $final_url, $post_content);
+			$content =  preg_replace('/\s'.$keyword.'\s/iu', $final_url, $post_content);
 			
 		}
 		
